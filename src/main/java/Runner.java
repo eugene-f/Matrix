46a14c254b0605ba1592774af200bf409f64a24c
Yevgeniy Frolov <eugene.frolov.95@gmail.com>
2015-02-20 16:22:44 +0600

    Add array creator

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,23 +1,19 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Runner {
 
     public static void main(String[] args) {
-        int[][] matrix = new int[10][];
+        int[][] matrix = new int[10][10];
 
         for (int i = 0; i < matrix.length; i++) {
-            matrix[i] = new int[10];
             for (int j = 0; j < matrix.length; j++) {
-                matrix[i][j] = -i;
+                matrix[i][j] = i;
             }
         }
 
         for (int i = 0; i < matrix.length; i++) {
-            System.out.println(matrix[i]);
             for (int j = 0; j < matrix.length; j++) {
-                System.out.println(matrix[i][j]);
+                System.out.print(matrix[i][j] + "   ");
             }
+            System.out.println();
         }
 
     }
