2688148c1ee20d7b5179fdf34854e8b6efefbfe8
Yevgeniy Frolov <eugene.frolov.95@gmail.com>
2015-02-20 16:33:30 +0600

    Change array creator Add print array

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,19 +1,41 @@
 public class Runner {
 
     public static void main(String[] args) {
-        int[][] matrix = new int[10][10];
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                matrix[i][j] = i;
-            }
-        }
+        int aI = 5;
+        int aJ = 10;
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                System.out.print(matrix[i][j] + "   ");
+        int bI = 3;
+        int bJ = 7;
+
+        int[][] matrixA = new int[aI][aJ];
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA.length; j++) {
+                matrixA[i][j] = i;
+                System.out.print(matrixA[i][j] + "   ");
             }
             System.out.println();
+        }
+
+        System.out.println();
+
+        int[][] matrixB = new int[bI][bJ];
+        for (int i = 0; i < matrixB.length; i++) {
+            for (int j = 0; j < matrixB.length; j++) {
+                matrixB[i][j] = i;
+                System.out.print(matrixB[i][j] + "   ");
+            }
+            System.out.println();
+        }
+
+
+
+        int[][] matrixC = new int[aI][bJ];
+
+        for (int i = 0; i < matrixC.length; i++) {
+            for (int j = 0; j < matrixC.length; j++) {
+//                matrixC[i][j] = matrixA[][] * matrixB[][];
+            }
         }
 
     }
