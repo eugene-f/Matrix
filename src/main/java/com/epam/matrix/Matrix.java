7820c6be8ad380aa39cf383002da267cc25e0e8a
Yevgeniy Frolov <eugene.frolov.95@gmail.com>
2015-02-21 23:16:24 +0600

    Add more constructors

--- src/main/java/com/epam/matrix/Matrix.java
+++ src/main/java/com/epam/matrix/Matrix.java
@@ -9,13 +9,54 @@ public class Matrix {
     private int[][] values;
 
     public Matrix(int rows, int columns) {
-        try {
+        create(rows, columns);
+    }
+
+    public Matrix(int[][] values) {
+        rows = valuesGetRows(values);
+        columns = valuesGetColumns(values);
+        int[][] v = new int[rows][columns];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                v[i][j] = values[i][j];
+            }
+        }
+        this.values = v;
+    }
+
+    public Matrix(int rows, int columns, int[][] values) {
+        if (values.length == rows && values[0].length == columns) {
+            create(rows, columns);
+            this.values = values;
+        } else {
+            System.out.println("ERROR: Dimension of the matrix does not coincide with the announced");
+        }
+    }
+
+    private void create(int rows, int columns) {
+        if (rows != 0 && columns !=0) {
             this.rows = rows;
             this.columns = columns;
-            this.values = new int[rows][columns];
-        } catch (NegativeArraySizeException e) {
-            System.out.println("ERROR: NegativeArraySizeException");
+            try {
+                this.values = new int[rows][columns];
+            } catch (NegativeArraySizeException e) {
+                System.out.println("ERROR: The dimension of the matrix can not be a negative number");
+            }
+        } else {
+            System.out.println("WARNING: Empty matrix can not be created");
         }
+    }
+
+    private int valuesGetRows(int[][] values) {
+        return values.length;
+    }
+
+    private int valuesGetColumns(int[][] values) {
+        int valuesColumns = 0;
+        for (int i = 0; i < valuesGetRows(values); i++) {
+            valuesColumns = Math.max(valuesColumns, values[i].length);
+        }
+        return values.length;
     }
 
     public static Matrix multiply(Matrix matrixA, Matrix matrixB) {
@@ -29,9 +70,17 @@ public class Matrix {
                 }
             }
         } else {
-            System.out.println("ERROR: matrixA.columns != matrixB.rows");
+            System.out.println("ERROR: The matrices of this size can not be multiplied");
         }
         return matrixC;
+    }
+
+    public void initZero() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                this.values[i][j] = 0;
+            }
+        }
     }
 
     public void initRandom() {
@@ -42,6 +91,18 @@ public class Matrix {
         }
     }
 
+    public Matrix resize(int rows, int columns) {
+        Matrix r = new Matrix(rows, columns);
+        int minRows = Math.min(this.rows, rows);
+        int minColumns = Math.min(this.columns, columns);
+        for (int i = 0; i < minRows; i++) {
+            for (int j = 0; j < minColumns; j++) {
+                r.values[i][j] = values[i][j];
+            }
+        }
+        return r;
+    }
+
     public void print() {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
@@ -50,6 +111,14 @@ public class Matrix {
             System.out.println();
         }
         System.out.println();
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
     }
 
 }
--- src/main/java/com/epam/matrix/Runner.java
+++ src/main/java/com/epam/matrix/Runner.java
@@ -4,12 +4,23 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        Matrix matrixA = new Matrix(3, 4);
+//        Matrix matrixA = new Matrix(3, 4);
+        Matrix matrixA = new Matrix(3, 4, new int[][]{
+                {0, 7, 2, 7},
+                {3, 3, 6, 7},
+                {3, 4, 5, 4}
+        });
         matrixA.initRandom();
         System.out.println("Matrix A");
         matrixA.print();
 
-        Matrix matrixB = new Matrix(4, 5);
+//        Matrix matrixB = new Matrix(4, 5);
+        Matrix matrixB = new Matrix(4, 5, new int[][]{
+                {5, 4, 5, 5, 6},
+                {7, 4, 1, 3, 7},
+                {5, 7, 5, 4, 7},
+                {2, 3, 0, 3, 4}
+        });
         matrixB.initRandom();
         System.out.println("Matrix B");
         matrixB.print();
@@ -17,6 +28,19 @@ public class Runner {
         Matrix matrixC = Matrix.multiply(matrixA, matrixB);
         System.out.println("Matrix C");
         matrixC.print();
+
+        matrixC.resize(matrixC.getRows() - 3, matrixC.getColumns() - 3).print();
+
+        Matrix matrixT = new Matrix(new int[][]{
+                {5, 4, 5, 6},
+                {7, 4, 1, 3, 7},
+                {5, 7, 5, 4, 7},
+                {2, 3, 0, 3, 4}
+        });
+        System.out.println("Matrix T");
+        matrixT.print();
+        System.out.println("rows: " + matrixT.getRows());
+        System.out.println("columns: " + matrixT.getColumns());
 
     }
 }
